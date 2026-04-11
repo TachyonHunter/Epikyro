e01c358b5f16472df36bb9beeab5cdd21d7c520a
tachyonhuntr <tachyonhuntr@gmail.com>
2026-04-11 22:17:21 +0400

    Testing from Pycharm

--- login.py
+++ login.py
@@ -2,7 +2,7 @@
 # incorrectUN - Incorrect Username passed
 # incorrectPW - Incorrect Password passed
 # correct - Success#
-# @PreciselyUNkNowN what case scheme do you want to use in this project??
+# what case scheme do you want to use in this project??
 # I was thinking camelCase for variables and functions, and PascalCase for classes, unless we aren't using classes?
 
 def login(username, password, userDB): #username, password, and the database with users.
