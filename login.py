54557f1b81b7c76cc92c501ea4fb08315f48eebb
PreciselyUNkNowN <natenj2511@gmail.com>
2026-04-11 19:29:57 +0400

    Most basic login function

--- login.py
+++ login.py
@@ -1,1 +1,11 @@
-tempVar = 'test'
+# Error codes:
+# incorrectUN - Incorrect Username passed
+# incorrectPW - Incorrect Password passed
+# correct - Success
+def login(username, password, userDB):
+    if username not in userDB:
+        return 'incorrectUN'
+    if password != userDB[username]:
+        return 'incorrectPW'
+    else:
+        return 'correct'
