9a46c649faafd4729256b9974efa67e6dcb215bb
PreciselyUNkNowN <natenj2511@gmail.com>
2026-04-11 19:33:30 +0400

    More commenting...

--- login.py
+++ login.py
@@ -2,7 +2,7 @@
 # incorrectUN - Incorrect Username passed
 # incorrectPW - Incorrect Password passed
 # correct - Success
-def login(username, password, userDB):
+def login(username, password, userDB): #username, password, and the database with users.
     if username not in userDB:
         return 'incorrectUN'
     if password != userDB[username]:
