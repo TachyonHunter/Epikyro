b577fcb6b5999c0dfc4373d30f631c0d62b6d2e2
PreciselyUNkNowN <natenj2511@gmail.com>
2026-04-12 18:05:50 +0400

    Stuff

--- loginPage.py
+++ loginPage.py
@@ -10,7 +10,7 @@ root.title('Login')
 mainframe = ttk.Frame(root, padding="3 3 12 12")
 mainframe.grid(column=0, row=0, sticky='N W E S')
 
-# Creates the username and password entries (user-input textboxes.
+# Creates the username and password entries (user-input textboxes).
 username = StringVar()
 password = StringVar()
 usernameField = Entry(mainframe, textvariable=username)
@@ -18,4 +18,8 @@ passwordField = Entry(mainframe, textvariable=password, show='*')
 usernameField.grid(row=2, column=2, sticky='N W E S')
 passwordField.grid(row=4, column=2, sticky='N W E S')
 
+# Creating labels.
+Label(mainframe, text='Login:', font=('Segoe UI', 25)).grid(row=0, column=2, sticky='N W E S')
+Label(mainframe, text='Username:', font=('Segoe UI', 13)).grid(row=1, column=2, sticky='N W E S')
+Label(mainframe, text='Password:', font=('Segoe UI', 13)).grid(row=3, column=2, sticky='N W E S')
 root.mainloop()
