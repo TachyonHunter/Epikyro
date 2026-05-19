7af29f6f5f25132b9ea26eecd980445f4f8a964d
tachyonhuntr <tachyonhuntr@gmail.com>
2026-05-19 21:12:15 +0400

    DatabaseTools

---
+++ DatabaseTools.py
@@ -0,0 +1,10 @@
+﻿import sqlite3
+def userList():
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT name FROM users")
+    users = [i[0] for i in cursor.fetchall()]
+    cursor.close()
+    conn.close()
+    return users
+
