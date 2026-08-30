c753cc30062af90cf1e96256b3bc39f0fc262b83
tachyonhuntr <tachyonhuntr@gmail.com>
2026-08-30 11:19:27 +0400

    Search Query basic logic

--- tempFileForRandomCode.py
+++ tempFileForRandomCode.py
@@ -1,3 +1,4 @@
+import sqlite3
 # welcomeNotification.set(f'Welcome {username.get()}!')
 #             loginButton.grid_remove()
 #             switchAccountButton.grid(row=0, column=0, sticky='E', padx=(0,4))
@@ -10,3 +11,10 @@
 #                 generalFrame.grid(row=0, column=1, sticky='E', padx=(0, 4))
 #                 headerFrame.columnconfigure(1, weight=1)
 #                 adminFrame.grid_remove()
+
+
+def searchFilter(Query: str, Filters: list):
+    with sqlite3.connect('users.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute(f"SELECT * FROM users WHERE username = '{Query}' AND " + " AND ".join([f"tags LIKE '%{i}%'" for i in Filters]))
+        return cursor.fetchall()
